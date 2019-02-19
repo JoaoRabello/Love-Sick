1f3b58215f0317eed7bfd4eb257385a4c880e819
Joao Rabello <rabellois@hotmail.com>
2019-02-19 14:23:39 -0300

    Spawner System
    -Created a script called Spawner to control and manage all enemy spawners, including the boss.

---
+++ Love Sick/Assets/Scripts/Managers/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField] GameObject[] enemies = new GameObject[3];
+    [SerializeField] Transform[] portals = new Transform[4];
+
+    private void Start()
+    {
+        StartCoroutine(FirstTypeWaveSpawn());
+    }
+
+    void Spawn(GameObject enemy, Transform portal)
+    {
+        Instantiate(enemy, portal.position, Quaternion.identity);
+    }
+
+    IEnumerator FirstTypeWaveSpawn()
+    {
+        yield return new WaitForSecondsRealtime(20f);
+        Spawn(enemies[0], portals[0]);
+        yield return new WaitForSecondsRealtime(2f);
+        Spawn(enemies[0], portals[0]);
+        yield return new WaitForSecondsRealtime(2f);
+        Spawn(enemies[0], portals[0]);
+    }
+}
